b0274079e0a91ef6badd4ef7c2adde25014fde94
Dieguidev <diegogaraycullas@gmail.com>
2024-06-16 12:53:49 -0500

    mejorado el template de crear.pug para que muestre el nombre de la calle

--- js/mapa.js
+++ js/mapa.js
@@ -1,15 +1,45 @@
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
-    async function (position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-
+    function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
       const mapa = L.map("mapa").setView([latitude, longitude], 16);
+      let marker;
+
+      //Utilizar Provider y Geocoder
+      const geocodeService = L.esri.Geocoding.geocodeService();
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(mapa);
+
+      //El pin
+      marker = new L.marker([latitude, longitude], {
+        draggable: true,
+        autoPan: true,
+      }).addTo(mapa);
+
+      marker.on("moveend", function (e) {
+        marker = e.target;
+        const position = marker.getLatLng();
+        mapa.panTo(new L.LatLng(position.lat, position.lng));
+
+        //obtener la info de las calles
+        geocodeService
+          .reverse()
+          .latlng(position, 13)
+          .run(function (error, result) {
+            marker.bindPopup(result.address.LongLabel);
+            //llenar los campos
+            document.querySelector(".street").textContent =
+              result?.address?.Address ?? "";
+            document.querySelector("#street").value =
+              result?.address?.Address ?? "";
+            document.querySelector("#lat").value = result?.latlng?.lat ?? "";
+            document.querySelector("#lng").value = result?.latlng?.lng ?? "";
+          });
+      });
     },
     function (error) {
       switch (error.code) {
