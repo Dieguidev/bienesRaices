8bb528a168ff15ed8ce625c84608b293edd753cb
Dieguidev <diegogaraycullas@gmail.com>
2024-06-16 15:27:04 -0500

    creados modelos en prisma propeties prices categories

---
+++ src/seeders/categories.ts
@@ -0,0 +1,37 @@
+const categories = [
+  {
+    name: 'Casa'
+  },
+  {
+    name: 'Departamento'
+  },
+  {
+    name: 'Bodega'
+  },
+  {
+    name: 'Terreno'
+  },
+  {
+    name: 'Edificio'
+  },
+  {
+    name: 'Cabaña'
+  },
+  {
+    name: 'Local'
+  },
+  {
+    name: 'Oficina'
+  },
+  {
+    name: 'Consultorio'
+  },
+  {
+    name: 'Habitacion'
+  },
+  {
+    name: 'Garage'
+  },
+
+
+]
